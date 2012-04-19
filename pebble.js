85d73933a7e8a79348bfa91c521d5a747ee300a2
Robert Wertman <robert.wertman@gmail.com>
2012-04-19 15:41:33 -0400

    Moved to a forward-moving parser.

--- pebble.js
+++ pebble.js
@@ -18,103 +18,68 @@ var indexOf = (function() {
 	};
 }());
 
-/* Checks to see if the charater at the given index is escaped.
- * Returns 0 if it is not escaped, or the number of slashes escaping the value
- * if it is escaped.
- */
-function isEscaped(selector, index) {
-	// Last possible index for an escape.
-	var lastSlash = index - 1;
-	// Loop backwards through the slashes.
-	while (lastSlash >= 0 && selector.charAt(lastSlash) === '\\') {
-		lastSlash--;
-	}
-	// Check the number of escaped escape characters.
-	if ((index - lastSlash) & 1) {
-		// If it's odd, we're not escaped, and we should break
-		return 0;
-	} else {
-		// If it's even, we're escaped, and we should keep going.
-		return (index - lastSlash);
-	}
-}
-
 // Find the next section of a compound from the end of a given point.
-function lexFromEnd(selector, last) {
-	var start = last,
+function lex(selector, start) {
+	var last = start,
 		inQuotedSection = false,
-		lexing = true;
-	while (lexing && last >= 0) {
-		var character = selector.charAt(last),
-			escaped;
+		lexing = true,
+		selectorLength = selector.length;
+	while (lexing && last < selectorLength) {
+		var character = selector.charAt(last);
 
 		switch (character) {
+		case '\\':
+			last += 2;
+			break;
 		case '#':
 		case '.':
 		case ':':
 			if (inQuotedSection) {
-				last--;
+				last++;
+			} else 	if (last === start) {
+				last++;
 			} else {
-				escaped = isEscaped(selector, last);
-				if (!escaped) {
-					last--;
-					lexing = false;
-				}
-				last -= escaped;
+				lexing = false;
 			}
 			break;
 
 		case '!':
 		case ',':
-		case ')':
-		case '(':
 		case '[':
-		case ']':
 			if (inQuotedSection) {
-				last--;
+				last++;
 			} else {
-				escaped = isEscaped(selector, last);
-				if (!escaped && start === last) {
-						last--;
+				if (start === last) {
+						last++;
+				} else {
+					lexing = false;
 				}
-				lexing = !(!escaped);
-				last -= escaped;
 			}
 			break;
-
 		case '~':
 		case '>':
 		case '+':
 		case ' ':
 			if (inQuotedSection) {
-				last--;
+				last++;
 			} else {
-				escaped = isEscaped(selector, last);
-				if (!escaped && start === last) {
-					lexing = false;
-					last--;
+				if (start === last) {
 					// We should scan for selectors to prevent
 					// [" ", "+", " "]-esque things as showing up as the next
 					// few symbols
-					var seeking = true;
-					while (seeking && last > 0) {
-						var next = lexFromEnd(selector, last);
-						if (/^\s*[+~ >]\s*$/.test(selector.slice(next + 1, last + 1))) {
-							last--;
-						} else {
-							seeking = false;
-						}
-					}
-				} else if (!escaped) {
 					lexing = false;
-				} else {
-					last -= escaped;
+					last++;
+					var next = lex(selector, last);
+					if (/^\s*[+~ >]\s*$/.test(selector.slice(last, next))) {
+						last = next;
+					}
 				}
+				lexing = false;
 			}
 			break;
 		// We haven't hit a new section yet.
 		default:
-			last--;
+			last++;
 			break;
 		}
 	}
@@ -132,15 +97,19 @@ var _pebble = window["pebble"],
 	// Get those nasty spaces gone.
 	selector = selector.trim();
 
-	var last = selector.length - 1;
+	var len = selector.length,
+		first = 0;
 
-	while (last >= 0) {
-		var first = lexFromEnd(selector, last),
-			token = selector.slice(first + 1, last + 1);
+	while (first < len) {
+		var last = lex(selector, first),
+			token = selector.slice(first, last);
 		if (token.length > 0) {
-			console.log('"' + token.trim() + '"');
+			console.log('"' + token + '"');
+		} else {
+			console.log("Breaking out because of an empty match.");
+			first = len;
 		}
-		last = first;
+		first = last;
 	}
 };
 
